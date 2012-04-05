356e68721b6fbf64f07bc495e70440651f98754e
Matt Ripley <matt@engr2-1-192-107-dhcp.int.colorado.edu>
2012-04-05 11:01:16 -0600

    added constructors for imageFetchers

--- src/mosaicmaker/DirectoryFetcher.java
+++ src/mosaicmaker/DirectoryFetcher.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class DirectoryFetcher extends ImageFetcher {
 
+	public DirectoryFetcher(){
+		
+		
+	}
+	
 	@Override
 	public ArrayList<Block> getReplacementImages(String URI) {
 		// TODO Auto-generated method stub

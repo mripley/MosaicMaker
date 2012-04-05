7e5420d3ade10c04ba771516f1f4bd49e6e26445
Your Name <alexandra.boughton@gmail.com>
2012-04-05 11:00:33 -0600

    adding ImageFetcher to MosaicMaker

--- src/mosaicmaker/MosaicMaker.java
+++ src/mosaicmaker/MosaicMaker.java
@@ -10,13 +10,22 @@ public class MosaicMaker {
 	
 	private BufferedImage sourceImage;
 	private int xBlockSize, yBlockSize;
+	private ImageFetcher fetcher;
 	
 	public MosaicMaker(String sourceImagePath, int xBlockSize, int yBlockSize, String replacementPath) throws IOException{
 		sourceImage = ImageIO.read(new File(sourceImagePath));
 		this.xBlockSize = xBlockSize;
 		this.yBlockSize = yBlockSize;
+		File dir = new File(replacementPath);
+		if(dir.list() == null){
+			this.fetcher = new GoogleImageFetcher();
+		}
+		else{
+			this.fetcher = new DirectoryFetcher();
+		}
+		
 	}
-	
+
 	private void makeMosaic(){
 		
 	}

fd98d3f93fa6d10fd43d593b8f0d634a23140d7e
Matt Ripley <matt@engr2-1-192-81-dhcp.int.colorado.edu>
2012-04-12 18:08:05 -0600

    Added the rest of main

--- src/mosaicmaker/Main.java
+++ src/mosaicmaker/Main.java
@@ -1,15 +1,35 @@
 package mosaicmaker;
 
+import java.io.IOException;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		if(args.length < 5){
+		if(args.length < 6){
 			System.out.println("Not enough args to MosaicMaker");
 			return;
 		}
+		
+		String sourceImage = args[1];
+		String outputImage = args[2];
+		String replacementPath = args[3];
+		int numXBlocks = Integer.valueOf(args[4]);
+		int numYBlocks = Integer.valueOf(args[5]);
+		
+		MosaicMaker maker;
+		try {
+			maker = new MosaicMaker(sourceImage, replacementPath);
+			maker.makeMosaic(numXBlocks, numYBlocks, outputImage);
+		} catch (IOException e) {
+			System.out.println("caught io exception in constructor of MosaicMaker");
+		}catch (MosaicMakerException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		
 	}
 
 }

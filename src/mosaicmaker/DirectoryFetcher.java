421def7564fb7bb370bdba69756bb2b03293307f
Matt Ripley <matt@engr2-1-192-217-dhcp.int.colorado.edu>
2012-04-25 17:15:50 -0600

    fixed slight git tree issue

--- src/mosaicmaker/DirectoryFetcher.java
+++ src/mosaicmaker/DirectoryFetcher.java
@@ -11,9 +11,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import javax.imageio.ImageIO;
-
-import org.imgscalr.Scalr;
 
 public class DirectoryFetcher extends ImageFetcher {
 
--- src/mosaicmaker/ImageFetcher.java
+++ src/mosaicmaker/ImageFetcher.java
@@ -15,15 +15,20 @@ public abstract class ImageFetcher {
 	
 	protected ArrayList<ReplacementBlock> replacements;
 	
-	// get the distance between 2 colors 
+	// get the distance between 2 colors in RGB space
 	private double getDist(Color c1, Color c2){
 		return Math.sqrt(Math.pow((c1.getRed()-c2.getRed()),2) + 
 								Math.pow((c1.getBlue()-c2.getBlue()), 2) + 
 								Math.pow((c1.getGreen()-c2.getGreen()),2));
 	}
 	
+	
+	// override this method to make new types of image fetchers. 
 	public abstract void loadReplacementImages(int xBlockSize, int yBlockSize);
 	
+	// gets the best replacement block. For now uses the min distance between the 
+	// query block and a block in the replacements array
+	// at some point in the future replace this with a better data structure. 
 	public ReplacementBlock getBestReplacementBlock(Color color, boolean duplicateImages){
 		ReplacementBlock retval = null; 
 		

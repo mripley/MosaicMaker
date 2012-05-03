005d38492319c2443dc4bedf2f299bee9d4e0262
Matthew Ripley <mattew.ripley@gmail.com>
2012-05-03 10:44:02 -0600

    Updated shell script

--- launch.sh
+++ launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -Xmx4096m -classpath .:bin/:./lib/imgscalr-lib-4.2.jar:./lib/bing-search-java-sdk.jar:./lib/gson-1.4.jar mosaicmaker.Main ./test_images/testImage2.jpg ./testOutput.png ./test_images/dir_test/ 200 100
+java -Xmx4096m -classpath .:bin/:./lib/imgscalr-lib-4.2.jar:./lib/bing-search-java-sdk.jar:./lib/gson-1.4.jar mosaicmaker.Main $1 $2 $3 $4 $5 
